5b870516149263db2147a616e3bb670e579868d2
Ramiro Garces <ramgarces24@gmail.com>
2023-08-21 08:56:37 -0500

    Add fixed width and temporary border to squares container

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const body = document.querySelector("body");
 const squaresContainer = document.querySelector(".squares-container");
 
+squaresContainer.style.width = "50%";
+squaresContainer.style.border = "1px solid red";
