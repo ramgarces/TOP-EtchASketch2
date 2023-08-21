215c83abdb33e35748d80665f5adfe049ecc6c8b
Ramiro Garces <ramgarces24@gmail.com>
2023-08-21 11:14:01 -0500

    Change addSquares() to create (numSquares * numSquares) amount of squares

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const body = document.querySelector("body");
 const squaresContainer = document.querySelector(".squares-container");
 
 const height = 1000;
+const numSquares = 16;
 squaresContainer.style.width = `${height}px`;
 squaresContainer.style.border = "1px solid red";
 squaresContainer.style.display = "flex";
 
 function addSquares() {
-    for(let i = 0; i < 16; i++) {
+    for(let i = 0; i < numSquares * numSquares; i++) {
         let newSquare = document.createElement("div");
         newSquare.style.border = "1px solid black";
         newSquare.style.margin = "0";
