d9ac77e7cf26db590021a56890316dc7c8553810
Ramiro Garces <ramgarces24@gmail.com>
2023-08-21 14:43:10 -0500

    Add height to addSquares() function for new squares

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ squaresContainer.style.width = `${width}px`;
 squaresContainer.style.height = `${height}px`;
 
 function addSquares() {
-    for(let i = 0; i < (numSquares - 1) ** 2; i++) {
+    for(let i = 0; i < numSquares * numSquares; i++) {
         let newSquare = document.createElement("div");
         newSquare.style.border = "1px solid grey";
         newSquare.style.flex = "1 1 auto";
         newSquare.style.width = `${width / numSquares}px`;
+        newSquare.style.height = `${height / numSquares}px`
         squaresContainer.appendChild(newSquare);
     }
 }
