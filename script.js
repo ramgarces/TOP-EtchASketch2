b90bf292c88b33808c0e11c40d0bafbfc6045276
Ramiro Garces <ramgarces24@gmail.com>
2023-08-21 13:39:12 -0500

    Add temporary height and width to squares

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ const width = 500;
 const height = width;
 const numSquares = 16;
 squaresContainer.style.width = `${width}px`;
-
+squaresContainer.style.height = `${height}px`;
 squaresContainer.style.border = "1px solid red";
 squaresContainer.style.display = "flex";
 squaresContainer.style.flexWrap = "wrap";
-squaresContainer.style.justifyContent = "space-between";
 
 function addSquares() {
-    for(let i = 0; i < numSquares * numSquares; i++) {
+    for(let i = 0; i < numSquares ** 2; i++) {
         let newSquare = document.createElement("div");
         newSquare.style.border = "1px solid black";
         newSquare.style.margin = "0";
+        newSquare.style.width = `${width / numSquares - 1}px`;
+        newSquare.style.height = `${height / numSquares - 1}px`;
         squaresContainer.appendChild(newSquare);
     }
 }
