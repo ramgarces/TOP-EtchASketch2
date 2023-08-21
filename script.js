ec8ee1720a76ec171f994c99edc33a475a3f1cf4
Ramiro Garces <ramgarces24@gmail.com>
2023-08-21 09:00:16 -0500

    Add function to create squares in squares container

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const squaresContainer = document.querySelector(".squares-container");
 
 squaresContainer.style.width = "50%";
 squaresContainer.style.border = "1px solid red";
+
+function addSquares() {
+    for(let i = 0; i < 16; i++) {
+        let newSquare = document.createElement("div");
+        newSquare.style.border = "1px solid black";
+        squaresContainer.appendChild(newSquare);
+    }
+}
+
+addSquares();
