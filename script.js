9b6d4ecc69058ed7ca79359cc95cc8897b76b0d3
Ramiro Garces <ramgarces24@gmail.com>
2023-08-21 11:21:12 -0500

    Add flex-wrap and justify-content properties to squarescontainer

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const body = document.querySelector("body");
 const squaresContainer = document.querySelector(".squares-container");
 
-const width = 1000;
+const width = 500;
 const numSquares = 16;
 squaresContainer.style.width = `${width}px`;
 squaresContainer.style.border = "1px solid red";
 squaresContainer.style.display = "flex";
+squaresContainer.style.flexWrap = "wrap";
+squaresContainer.style.justifyContent = "space-between";
 
 function addSquares() {
     for(let i = 0; i < numSquares * numSquares; i++) {
         let newSquare = document.createElement("div");
         newSquare.style.border = "1px solid black";
         newSquare.style.margin = "0";
-        newSquare.style.padding = "0";
         squaresContainer.appendChild(newSquare);
     }
 }
